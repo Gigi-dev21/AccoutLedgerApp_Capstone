813d433fc88d8a990915a4127b698b224f2e40d6
embet-mengistu <131568421+embet-mengistu@users.noreply.github.com>
2025-04-29 11:54:42 -0700

    Called the homescreen in the main file

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,8 @@
+import java.io.FileNotFoundException;
+
 public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        HomeScreen homeScreen = new HomeScreen();
+        homeScreen.displayHomeScreen();
+    }
 }
