ffbbb5860662d02ed99766b5c3b67abdadaa5c2c
embet-mengistu <131568421+embet-mengistu@users.noreply.github.com>
2025-04-29 12:02:32 -0700

    created the makepayment method

--- src/AccountTransactions.java
+++ src/AccountTransactions.java
@@ -51,4 +51,41 @@ public class AccountTransactions {
             System.out.println("Returning to Home screen, Please re-enter the correct information.");
         }
     }
+
+
+    public void makePayment() {
+        System.out.println("-- You chose to make payment. --");
+        System.out.print("Please enter vendor name: ");
+        String vendorNameEntered = scanner.nextLine();
+
+        System.out.print("Please enter the amount you want to pay: ");
+        double amountEntered = scanner.nextDouble();
+        scanner.nextLine();
+
+        System.out.print("Please enter a description: ");
+        String descriptionEntered = scanner.nextLine();
+
+        System.out.println(
+                "Date: " + formattedDate + "\n" +
+                        "Time: " + formattedTime + "\n" +
+                        "Vendor Name: " + vendorNameEntered + "\n" +
+                        "Amount: $" + amountEntered + "\n" +
+                        "Description: " + descriptionEntered + "\n" +
+                        "\nIs this information correct? (Y/N): "
+        );
+        String confirm = scanner.nextLine().trim().toLowerCase();
+
+        if (confirm.equalsIgnoreCase("y")) {
+            try {
+                FileWriter fileWriter = new FileWriter("transactions.csv", true); // append mode
+                fileWriter.write(formattedDate + "|" + formattedTime + "|" + descriptionEntered + "|" + vendorNameEntered + "|" + "-" + amountEntered + "\n");
+                fileWriter.close();
+                System.out.println("✅ Payment made successfully!");
+            } catch (Exception e) {
+                System.out.println("An error occurred while saving the deposit: " + e.getMessage());
+            }
+        } else {
+            System.out.println("Returning to Home screen, Please re-enter the correct information.");
+        }
+    }
 }
